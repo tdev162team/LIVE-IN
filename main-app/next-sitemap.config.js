2ba808e2ed9f2786217aa0d2466cf8deacbfca30
thanasis162 <willbedeleted55@gmail.com>
2025-08-26 22:42:09 +0300

    Add next-sitemap and postbuild script

---
+++ main-app/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://live-in-three.vercel.app/', // άλλαξε το με το δικό σου domain
+  generateRobotsTxt: true, // θα δημιουργήσει και robots.txt
+  sitemapSize: 5000, // προαιρετικό, χωρίζει μεγάλο sitemap σε πολλαπλά αρχεία
+};
